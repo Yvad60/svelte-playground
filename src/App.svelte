919d222435cad7b8f22efae42859c65dda689540
Ivad Yves HABIMANA <54445417+Yvad60@users.noreply.github.com>
2023-03-26 23:02:50 +0200

    Add counter app

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  let count = 0;
+</script>
+
+<main class="bg-slate-200 flex justify-center items-center h-screen text-white">
+  <button on:click={() => count++} class="px-5 py-2 bg-slate-500 rounded-md"
+    >Increment +</button
+  >
+  <h1 class="mx-8 text-black text-3xl">{count}</h1>
+  <button on:click={() => count--} class="px-5 py-2 bg-red-400 rounded-md"
+    >decrement -</button
+  >
+</main>
